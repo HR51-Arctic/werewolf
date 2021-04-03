967b1a2c32c0c038b979046986fbe7a25adea123
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-04-02 17:13:51 -0700

    added socket.io logic to backend

--- server/index.js
+++ server/index.js
@@ -1,16 +1,44 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 const app = express();
-const port = 3000;
 
+const port = process.env.PORT || 3000;
+const index = require('./routes/index');
 
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
+app.use(index);
 
+const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('server route works')
+const io = socketIo(server);
+
+// app.get('/', (req, res) => {
+//   res.send('server route works')
+// })
+
+let interval;
+io.on('connection', (socket) => {
+  console.log("New client connected");
+  if(interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getAPIAndEmit(socket), 1000);
+  socket.on('disconnect', () => {
+    console.log('client disconnected');
+    clearInterval(interval);
+  })
 })
 
-app.listen(port, () => {
+const getApiAndEmit = socket => {
+  const response = new Date();
+  socket.emit("FromAPI", response);
+}
+
+
+
+
+server.listen(port, () => {
   console.log(`Server listening on ${port}`)
-} )
+} );
