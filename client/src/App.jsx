4d94c371f1fbb04e1ee6e10f4cd60218c30a908d
Irene Hodge <irenehodge90@gmail.com>
2021-04-05 12:48:43 -0700

    lobby styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import socketIOClient from 'socket.io-client';
-import Lobby from './Lobby.jsx';
-import GameView from './GameView.jsx';
-const ENDPOINT = 'http://localhost:3000';
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+import Lobby from "./Lobby.jsx";
+import GameView from "./GameView.jsx";
+const ENDPOINT = "http://localhost:3000";
 
 function App() {
   const [dirtySock, setDirtySock] = useState({});
-  const [message, setMessage] = useState('');
-  const [gameState, setGameState] = useState('');
+  const [message, setMessage] = useState("");
+  const [gameState, setGameState] = useState("");
   const [lobbyParticipants, setLobbyParticipants] = useState([]);
   const [play, setPlay] = useState(false);
-  const [myId, setMyId] = useState('');
-
+  const [myId, setMyId] = useState("");
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
@@ -19,36 +18,34 @@ function App() {
     setDirtySock(socket);
     //  + console.log(dirtySock);
     // console.log(dirtySock)
-    socket.on('myId', (id) => {
+    socket.on("myId", (id) => {
       setMyId(id);
     });
 
-    socket.on('GetParticipants', (data) => {
+    socket.on("GetParticipants", (data) => {
       setLobbyParticipants(data);
     });
 
-
-    socket.on('PreGame', (gameState) => {
+    socket.on("PreGame", (gameState) => {
       setGameState(gameState);
       setPlay(true);
-
-    })
-
+    });
   }, []);
 
   const handleGameStart = () => {
-    dirtySock.emit('StartGame');
-  }
+    dirtySock.emit("StartGame");
+  };
   if (play) {
-    return <GameView myId={myId} gameState={gameState} />
+    return <GameView myId={myId} gameState={gameState} />;
   }
 
   return (
     <div>
-      <Lobby participants={lobbyParticipants} handleGameStart={handleGameStart.bind(this)} />
+      <Lobby
+        participants={lobbyParticipants}
+        handleGameStart={handleGameStart.bind(this)}
+      />
     </div>
-
-
   );
 }
 
--- client/src/Lobby.jsx
+++ client/src/Lobby.jsx
@@ -1,48 +1,49 @@
-import React, { useState } from 'react'
-import GameView from './GameView.jsx';
-
+import React, { useState } from "react";
+import GameView from "./GameView.jsx";
+import AppHeader from "./AppHeader.jsx";
 
 const Lobby = ({ participants, handleGameStart }) => {
-
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   return (
     <>
-      <div>
-        <div style={{ height: '250px', width: '50%', border: '3px solid black' }}>
-          <span>Username and stats</span>
-        </div>
-        <div style={{ height: '150px', width: '100px', border: '3px solid black' }}>
-          {participants.map((player) => {
-            return (
-              <div key={player}>{player}</div>
-            )
-          })}
-        </div>
+      <div id="lobby">
+        <AppHeader id="header" />
         <div>
-          <div style={{ height: '100px', width: '50%', border: '3px solid black' }}>
-            <textarea
-              name='chat'
-              type='text'
-              value={message}
-              onChange={e => setMessage(e.target.value)}
-            >Message</textarea>
+          <div>
+            <div>Username and stats</div>
           </div>
-          <button
-            type='submit'
-            value='Submit'
-            onClick={() => console.log('Posted message')}
-          >Post Message</button>
+          <div id="players">
+            {participants.map((player) => {
+              return <div key={player}>{player}</div>;
+            })}
+          </div>
+          <div>
+            <div id="lobby-chat">
+              <form>
+                <input
+                  name="chat"
+                  type="text"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                ></input>
+              </form>
+            </div>
+            <button
+              type="submit"
+              value="Submit"
+              onClick={() => console.log("Posted message")}
+            >
+              Post Message
+            </button>
+          </div>
         </div>
+        <button type="submit" value="Submit" onClick={() => handleGameStart()}>
+          PLAY
+        </button>
       </div>
-      <button
-        type='submit'
-        value='Submit'
-        onClick={() => handleGameStart()}
-      >PLAY</button>
     </>
-  )
-}
-
+  );
+};
 
 export default Lobby;
