a51639f9af68c64652ef2c77db0954fb447c76c4
Darian Chan <darian.s.chan@gmail.com>
2021-04-05 12:00:13 -0700

    wrote logic to assign roles

--- server/index.js
+++ server/index.js
@@ -41,14 +41,70 @@ io.on('connection', (socket) => {
   socket.on('StartGame', () => {
     // this is only available if clients.length >= 7
     let werewolfCounter = 0;
-    var newGame = new Game();
-    currentGame = newGame
+    if (!currentGame) {
+      currentGame = new Game()
+
+    }
+
+    // var newGame = new Game(); ---> for futurue instancing for many game states. Right now single state for MVP
+    // currentGame = newGame
+
+
+
     // random role generator? so it can be added to newPlayer
-    clients.forEach(client => {
+    let playerPool = []
+    clients.forEach((client) => {
       var newPlayer = new Player(client);
-      newGame.players.push(newPlayer);
+      playerPool.push(newPlayer);
+
     })
-    io.sockets.emit('PreGame', newGame);
+    console.log(playerPool)
+    console.log(currentGame)
+
+
+
+    //get wolves
+    let wolvesCount
+    //determine how many wolves desired
+    if (playerPool.length <= 15) wolvesCount = 2
+    if (playerPool.length > 15) {
+      let additonalWolves = Math.floor((playerPool.length - 16) /4)
+      wolvesCount = 3 + additionalWolves
+    }
+
+    for (let x=0; x<wolvesCount; x++) {
+      let wolfIndex = Math.floor(Math.random() * playerPool.length)
+      let wolf = playerPool.splice(wolfIndex,1)[0]
+      wolf.role = 'wolf'
+      currentGame.players.push(wolf)
+    }
+    console.log(currentGame)
+    // const randomElement = array[Math.floor(Math.random() * array.length)];
+    let seirIndex = Math.floor(Math.random() * playerPool.length)
+    let seir = playerPool.splice(seirIndex,1)[0]
+    console.log("seri", seir)
+    seir.role = 'seir'
+    currentGame.players.push(seir)
+    console.log(currentGame)
+
+    //get doctor
+    let docIndex = Math.floor(Math.random() * playerPool.length)
+    let doc = playerPool.splice(docIndex,1)[0]
+    doc.role = 'doctor'
+    currentGame.players.push(doc)
+    console.log(currentGame)
+
+    //add rest
+    currentGame.players = [...currentGame.players, ...playerPool]
+    console.log(currentGame)
+    console.log(playerPool)
+
+// There is always one seer and one doctor.
+// When there are 15 or less players total, there are 2 werewolves.
+// When there are 16 players there are 3 werewolves and as the number of participants grow: there is one werewolf for every 4 players.
+// https://docs.google.com/document/d/182XhKE_ryuzE-wlKsUlBoRr5kgxEETCD7X7XNhxhkgY/edit
+// https://playwerewolf.co/pages/rules
+    io.sockets.emit('PreGame', currentGame);
   })
 })
 
