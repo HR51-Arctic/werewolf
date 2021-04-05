80d2b9820153203260733a9c1686cf4474d32c02
Chris McVey <chris.mcvey@gmail.com>
2021-04-05 11:33:53 -0700

    Pregame timer implemented

--- server/index.js
+++ server/index.js
@@ -68,10 +68,28 @@ io.on('connection', (socket) => {
       }
       newGame.players.push(newPlayer);
     })
+
     io.sockets.emit('PreGame', newGame);
+    //start timer
+    let preGameTimer = 5;
+    const preGameTimerLoop =
+      setInterval(() => {
+        preGameTimer -= 1;
+        io.sockets.emit('timer', preGameTimer);
+        console.log(preGameTimer);
+        if (preGameTimer == 0) {
+          clearInterval(preGameTimerLoop);
+        }
+      }, 1000);
+
+
+    //when it hits zero, emit to everyone to transition to night
+    //we start the nightfunction
+
   })
 })
 
+//night function
 
 server.listen(port, () => {
   console.log(`Server listening on ${port}`)
