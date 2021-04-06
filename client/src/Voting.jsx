ca3329fb0fa995dc40057df7339c80a1d554a5f2
danicastojanovic <dnc.stjnvc@gmail.com>
2021-04-06 14:44:59 -0700

    voting buttons

--- client/src/Voting.jsx
+++ client/src/Voting.jsx
@@ -1,5 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Voting = ({day, myId, werewolfvote  }) => {
-  // if day
+const Voting = ({ gameState, day, myId, vote, docChoice, role }) => {
+  // const [voting, setVoting] = useState(false);
+  let voting = false
+  let myPlayer = null
+  for (let x = 0; x < gameState.players.length; x++) {
+    let player = gameState.players[x]
+    if (myId === player.id) {
+      myPlayer = player
+      break
+    }
+  }
+  if (myPlayer.alive && day) {
+    voting = true
+  }
+  // else if (myPlayer.role === 'werewolf' && !day && myPlayer.alive) {
+  //   setVoting(true)
+  // }
+  return(
+    <div>
+      {gameState.players.map((player) => {
+        if (player.id !== myId && player.alive) {
+          return <button onClick={() => {vote(player.id)}}>{player.name}</button>
+        }
+      }) }
+    </div>
+  )
 }
+
+export default Voting;
+
+
+{/* //if not voting present this
+<div>
+  <select>
+  <option value='Lynch'>Lynch</option>
+  <option value='Save'>Save</option>
+  <option value='See'>See</option>
+  </select>
+  <button type='submit' value='Submit'>Submit</button>
+</div> */}
+
