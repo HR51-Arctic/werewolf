9faeee5f83aa5755c49988d12dd352cf4a30f821
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-04-08 15:14:51 -0700

    added timer settings to lobby

--- client/src/Lobby.jsx
+++ client/src/Lobby.jsx
@@ -4,13 +4,50 @@ import AppHeader from "./AppHeader.jsx";
 
 const Lobby = ({ participants, handleGameStart, loggedIn, gameSettings, onGameSettingsChange }) => {
   const [message, setMessage] = useState("");
+  let settingsForm = (<form
+  id='settingsView'
+> <label>
+  Timers:
+</label>
+  <label>
+    Pre-game:
+  <input
+    className="setting"
+    name="preGameTimer"
+    type="number"
+    value={gameSettings.preGameTimer}
+    onChange={onGameSettingsChange}
+  />
+  </label>
+  <label>
+    Day:
+  <input
+    className="setting"
+    name="dayTimer"
+    type="number"
+    value={gameSettings.dayTimer}
+    onChange={onGameSettingsChange}
+  />
+  </label>
+  <label>
+    Night:
+  <input
+    className="setting"
+    name="nightTimer"
+    type="number"
+    value={gameSettings.nightTimer}
+    onChange={onGameSettingsChange}
+  />
+  </label>
+</form>);
   return (
     <>
       <div id="frontPage">
         <AppHeader id="header" />
         <div id="lobby">
           <div id="players">
-            <h4 id="playerHeader">Players</h4>
+            <h4 id="playerHeader">Players</h4>{settingsForm}
+
             {participants.map((player) => {
               return (
                 <div id="indivPlayer" key={player.id}>
@@ -19,35 +56,6 @@ const Lobby = ({ participants, handleGameStart, loggedIn, gameSettings, onGameSe
               );
             })}
           </div>
-          <form>
-            <label>
-              Pre-game Timer Length
-            <input
-              name="preGameTimer"
-              type="number"
-              value={gameSettings.preGameTimer}
-              onChange={onGameSettingsChange}
-            />
-            </label>
-            <label>
-              Day Timer Length
-            <input
-              name="dayTimer"
-              type="number"
-              value={gameSettings.dayTimer}
-              onChange={onGameSettingsChange}
-            />
-            </label>
-            <label>
-              Night Timer Length
-            <input
-              name="nightTimer"
-              type="number"
-              value={gameSettings.nightTimer}
-              onChange={onGameSettingsChange}
-            />
-            </label>
-          </form>
           {participants.length >= 7 && loggedIn && (
             <button
               className="playButton"
