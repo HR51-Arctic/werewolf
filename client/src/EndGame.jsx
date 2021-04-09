52c58ef80e49e79e7c65f9e51a833ac88ee2a918
Chris McVey <chris.mcvey@gmail.com>
2021-04-08 19:48:09 -0700

    Added dripping blood to end game modal

--- client/src/EndGame.jsx
+++ client/src/EndGame.jsx
@@ -4,8 +4,12 @@ const EndGameModal = ({ endGame, clickHandler }) => {
   return (
     <div id="endGameBackground">
       <div id="endGameModal">
-        <div id="endGameMessage" className="blood">{endGame}
+        <div id="endGameMessage" className="blood">{endGame}</div>
+        <div id="bloodContainer">
+          <div className="drop one"></div><div className="drop two"></div><div className="drop three"></div><div className="drop four"></div>
         </div>
+
+        {/* <div className="wave"></div> */}
         <button id="endGameButton" onClick={() => clickHandler()}>Play Again</button>
       </div>
     </div>
