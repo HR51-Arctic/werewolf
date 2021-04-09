bfa1a5b7bd6f8b6be733bb8506926244ce5faf78
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-04-09 12:34:34 -0700

    commented out router and index serverside

--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ const path = require("path");
 // const favicon = require("serve-favicon");
 
 const port = process.env.PORT || 3000;
-const index = require("./routes/index");
+// const index = require("./routes/index");
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
-app.use(index);
+// app.use(index);
 // app.use(favicon(path.join(__dirname, "..", "client", "src", "images", "favicon.ico")));
 
 const server = http.createServer(app);
